ae57b4ad5bfa4d95168bb372b224961445f63434
Erik Luebbers <erluebbers@gmail.com>
2022-02-07 17:16:17 -0800

    commit tree 398d50ff6a19f7b5782624b188bc937ba2bf256d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.length
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else 
+      final_element = array.pop
+      first_elements = array.join(", ")
+      final_and = ", and "
+      first_elements + final_and + final_element
+    end
 end
